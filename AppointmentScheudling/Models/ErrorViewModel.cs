7eaa6f0b0f4e6ba3f2b57d45c81d1f632b03f5cf
Akhilesh <360akhileshsingh.it@gmail.com>
2023-09-28 00:29:00 +0530

    Add project files.

---
+++ AppointmentScheudling/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppointmentScheudling.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
