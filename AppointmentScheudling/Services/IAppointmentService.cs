2fbce08720543a10d362ef412cfa790b41c897c5
Akhilesh <360akhileshsingh.it@gmail.com>
2023-10-06 01:57:47 +0530

    Get Appointment Details API.

--- AppointmentScheudling/Services/IAppointmentService.cs
+++ AppointmentScheudling/Services/IAppointmentService.cs
@@ -9,5 +9,6 @@ namespace AppointmentScheudling.Services
         public Task<int> AddUpdate(AppointmentVM model);
         public List<AppointmentVM> DoctorsEventsById(string doctorId);
         public List<AppointmentVM> PatientsEventsById(string patientId);
+        public AppointmentVM GetById(int id);
     }
 }
