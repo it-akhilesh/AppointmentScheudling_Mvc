fbaf51c27aecd8367f0f14d9a1257479f07f6a0d
Akhilesh <360akhileshsingh.it@gmail.com>
2023-10-16 23:00:40 +0530

    Two added properties in the applicationuser.

--- AppointmentScheudling/Models/ApplicationUser.cs
+++ AppointmentScheudling/Models/ApplicationUser.cs
@@ -5,5 +5,7 @@ namespace AppointmentScheudling.Models
     public class ApplicationUser : IdentityUser
     {
         public string Name { get; set; }
+        public string Address { get; set; }
+        public  int PostalCode { get; set; }
     }
 }
